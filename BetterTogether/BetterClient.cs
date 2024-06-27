5a3c3ee4db03a691e207e43179ece2d0b663ea44
spreedated <21370839+spreedated@users.noreply.github.com>
2024-06-27 16:03:42 +0200

    cleanup

--- BetterTogether/BetterClient.cs
+++ BetterTogether/BetterClient.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
