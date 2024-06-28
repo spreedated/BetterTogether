f95f9304e03ec2b208423cdf933eec43d43f2bde
spreedated <21370839+spreedated@users.noreply.github.com>
2024-06-28 17:53:17 +0200

    added test project and signed assemblies

---
+++ BetterTogether/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("d08eb1f8-f188-41b6-8380-e9d658353290")]
+[assembly: InternalsVisibleTo("BetterTogether.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003d2fffed29ac536a1f58fc9eab70d1a410034519fc7be0b7d48de46a7709d40a19e3ffee2e417dd3590b16db9e18e2b019a375bdb78ac98a34da9d0d42a88d8e06179bb736370a80736e88f251cd52d048813500116e13c105ab3a556e245709a95aba4f9cb3aa284426d3a6769dcc3fa0ac6ea3611bf02268c9b776a9faf3d5")]
